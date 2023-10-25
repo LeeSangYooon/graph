4fc3949cc151d7efd58d481affae2ce65a763418
LeeSangYooon <56057552+LeeSangYooon@users.noreply.github.com>
2023-10-25 22:01:26 +0900

    5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,8 @@ import Interpreter.REPL
 import scala.annotation.tailrec
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    run(REPL.init)
-  }
+  def main(args: Array[String]): Unit = run(REPL.init)
+
   @tailrec
   private def run(repl: REPL): REPL = run(repl.prompt())
 }
